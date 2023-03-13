bfbad3fc0e0d8001bc609d24bba4758699d61f8b
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-03-13 19:04:59 +0900

    master

--- DiscordCalenders.py
+++ DiscordCalenders.py
@@ -2,12 +2,11 @@ import discord
 from discord.ext import commands
 import datetime
 
-bot = commands.Bot(command_prefix='!',intents=intents')
-
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # カレンダーに予定を追加するコマンド
 @bot.command()
-async def add_event(ctx, date_str, event):
+async def add_event(ctx, date_str, *, event):
     date = datetime.datetime.strptime(date_str, '%Y/%m/%d')
     calendar_channel = bot.get_channel(1234567890) # カレンダーを表示するチャンネルのIDを入力
     await calendar_channel.send(f'{date.date()}: {event}')
@@ -19,6 +18,6 @@ async def today_events(ctx):
     calendar_channel = bot.get_channel(1234567890) # カレンダーを表示するチャンネルのIDを入力
     async for message in calendar_channel.history(limit=None):
         if today == datetime.datetime.strptime(message.content.split(':')[0], '%Y-%m-%d').date():
-            await ctx.send(message.content)
+            await ctx.send(message.content.split(':')[1])
 
 bot.run('your_bot_token_here')
