6104bfdbaaffb096a2182a2694fe241ce0a4a464
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-03-28 20:05:29 +0900

    Update DiscordCalenders.py
    例

--- DiscordCalenders.py
+++ DiscordCalenders.py
@@ -8,16 +8,16 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @bot.command()
 async def add_event(ctx, date_str, *, event):
     date = datetime.datetime.strptime(date_str, '%Y/%m/%d')
-    calendar_channel = bot.get_channel(1234567890) # カレンダーを表示するチャンネルのIDを入力
+    calendar_channel = bot.get_channel(1084806810957000756) # カレンダーを表示するチャンネルのIDを入力
     await calendar_channel.send(f'{date.date()}: {event}')
 
 # 今日の予定を表示するコマンド
 @bot.command()
 async def today_events(ctx):
     today = datetime.datetime.now().date()
-    calendar_channel = bot.get_channel(1234567890) # カレンダーを表示するチャンネルのIDを入力
+    calendar_channel = bot.get_channel(1084806810957000756) # カレンダーを表示するチャンネルのIDを入力
     async for message in calendar_channel.history(limit=None):
         if today == datetime.datetime.strptime(message.content.split(':')[0], '%Y-%m-%d').date():
             await ctx.send(message.content.split(':')[1])
 
-bot.run('your_bot_token_here')
+bot.run('MTA4NDgwNjExOTMxMjA3Njg1MA.GHcaGu.cJG-jhGEFjIUgpjauj4P0GuvlgOJnv8eQzQDmI')
